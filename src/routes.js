e1eba9471341e97903919306d8678f38e4779331
Pedro Rodrigues <pedro.roguea@gmail.com>
2021-06-04 01:14:00 -0300

    Implementando prazo de entrega funcional

--- src/routes.js
+++ src/routes.js
@@ -13,6 +13,45 @@ const profile = {
     "vacation-per-year": 4
 }
 
+const Job = {
+    controllers: {
+        index(req, res) {
+            const updatedJobs = jobs.map((job) => {
+                // ajustes no job
+                const remaining = Job.services.remainingDays(job)
+                const status = remaining <= 0 ? 'done' : 'progress'
+
+                return {
+                    ...job,
+                    remaining,
+                    status,
+                    budget: profile["value-hour"] * job["total-hours"]
+                }
+            })
+
+            return res.render(views + "index", { jobs: updatedJobs })
+        },
+    },
+    services: {
+        remainingDays(job) {
+            // calculo de tempo restante
+            const remainingDays = (job["total-hours"] / job["daily-hours"]).toFixed()
+
+            const createdDate = new Date(job.created_at)
+            const dueDay = createdDate.getDate() + Number(remainingDays)
+            const dueDateInMs = createdDate.setDate(dueDay)
+
+            const timeDiffInMs = dueDateInMs - Date.now()
+            // transformar milli em dias
+            const dayInMs = 1000 * 60 * 60 * 24
+            const dayDiff = Math.floor(timeDiffInMs / dayInMs)
+
+            // restam x dias
+            return dayDiff
+        }
+    }
+}
+
 const jobs = [
     {
         id: 1,
@@ -30,21 +69,7 @@ const jobs = [
     }
 ]
 
-routes.get('/', (req, res) => {
-
-    const updatedJobs = jobs.map((job) => {
-        // ajustes no job
-        // calculo de tempo restante
-        const remainingDays = (job["total-hours"] / job["daily-hours"]).toFixed()
-
-        const createdDate = new Date(job.created_at)
-        const dueDay = createdDate.getDate() + Number(remainingDays)
-
-        return job
-    })
-
-    return res.render(views + "index", { jobs })
-})
+routes.get('/', Job.controllers.index)
 routes.get('/job', (req, res) => res.render(views + "job"))
 routes.post('/job', (req, res) => {
 
